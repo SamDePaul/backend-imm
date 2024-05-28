34d202c4c7d396a8f92f2cf051b64d2346362c6e
SamDePaull <prima2100016041@webmail.uad.ac.id>
2024-05-28 22:20:40 +0700

    ganti footer

--- resources/views/layouts/admin.blade.php
+++ resources/views/layouts/admin.blade.php
@@ -309,7 +309,7 @@
         <footer class="sticky-footer bg-white">
             <div class="container my-auto">
                 <div class="copyright text-center my-auto">
-                    <span>Copyright &copy; <a href="https://github.com/Estrella-1234" target="_blank">Estrella</a> {{ now()->year }}</span>
+                    <span>Copyright &copy; <a href="https://github.com/SamDePaul/backend-imm" target="_blank">IMM</a> {{ now()->year }}</span>
                 </div>
             </div>
         </footer>
